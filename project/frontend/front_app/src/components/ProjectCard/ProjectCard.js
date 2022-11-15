8a49ba4501d11fc014ca757c9590916af0e47f4d
unknown <2gnldud@gmail.com>
2022-11-16 01:51:11 +0900

    2022-11-16-01-51-HY

--- project/frontend/front_app/src/components/ProjectCard/ProjectCard.js
+++ project/frontend/front_app/src/components/ProjectCard/ProjectCard.js
@@ -3,9 +3,9 @@ import "./ProjectCard.scss"
 const ProjectCard = ({ title, img }) => {
     return (
         <div className="card col-4 m-3" >
-            <img src={img} class="card-img-top" alt="assets/temp_image.png" />
-            <div class="card-body">
-                <p class="card-text">{title}</p>
+            <img src={img} className="card-img-top" alt="assets/temp_image.png" />
+            <div className="card-body">
+                <p className="card-text">{title}</p>
             </div>
         </div>
     )
