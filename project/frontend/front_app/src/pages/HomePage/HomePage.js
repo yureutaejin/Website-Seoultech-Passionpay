9c6ffee427a70b4172db268e3604a7a33b871e71
unknown <2gnldud@gmail.com>
2022-11-15 20:57:15 +0900

    2022-11-15-20-57-HY

--- project/frontend/front_app/src/pages/HomePage/HomePage.js
+++ project/frontend/front_app/src/pages/HomePage/HomePage.js
@@ -3,9 +3,12 @@ import styles from "./HomePage.module.css";
 const HomePage = () => {
     return (
         <div>
-            <div className={styles.HomeIMG} />
-            {/* <img src="/assets/HomeIMG.png"></img> */}
-        </div>
+            <img src="assets/HomeIMG.png" className={styles.HomeIMG}></img>
+            <img src="assets/HomeLogo.svg" className={styles.HomeLogo}></img>
+            <div className={styles.HomeDesc}>서울과학기술대학교<br /><span className="fw-bold">산업정보시스템전공</span> 학술 동아리</div>
+            <div className={styles.Project_tag}>우리의,<br />우수 프로젝트</div>
+            <div className={styles.bar}></div>
+        </div >
     )
 }
 
