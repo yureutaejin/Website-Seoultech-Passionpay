c4566f414158f392f987a343d9832e3a56371bf1
unknown <2gnldud@gmail.com>
2022-11-16 10:34:17 +0900

    2022-11-16-10-34-HY

--- project/frontend/front_app/src/components/ProjectCard/ProjectCard.js
+++ project/frontend/front_app/src/components/ProjectCard/ProjectCard.js
@@ -1,12 +1,15 @@
-import "./ProjectCard.scss"
+import "./ProjectCard.css"
+import classNames from "classnames/bind"
 
-const ProjectCard = ({ title, img }) => {
+// const cx = classNames.bind(styles)
+
+const ProjectCard = ({ id, title, img, onClick }) => {
     return (
-        <div className="card col-4 m-3" >
-            <img src={img} className="card-img-top" alt="assets/temp_image.png" />
+        <div className="card col-4 m-3 px-0 cursor-pointer" onClick={onClick} >
             <div className="card-body">
-                <p className="card-text">{title}</p>
+                <h5 className="card-title"><div className="mb-1">{id}</div>{title}</h5>
             </div>
+            <img src={img} className="card-img-bottom" alt="assets/temp_image.png" />
         </div>
     )
 }
--- project/frontend/front_app/src/pages/HomePage/HomePage.js
+++ project/frontend/front_app/src/pages/HomePage/HomePage.js
@@ -1,19 +1,21 @@
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import "./HomePage.scss";
+import { useHistory } from "react-router"
 
 const HomePage = () => {
+    const history = useHistory()
     return (
         <div>
-            <img src="assets/HomeIMG.png" className="HomeIMG w-100"></img>
-            <img src="assets/HomeLogo.svg" className="HomeLogo"></img>
+            <img src="assets/HomeIMG.png" alt="assets/temp_image.png" className="HomeIMG w-100"></img>
+            <img src="assets/HomeLogo.svg" alt="assets/temp_image.png" className="HomeLogo"></img>
             <div className="HomeDesc">서울과학기술대학교<br /><span className="fw-bold">산업정보시스템전공</span> 학술 동아리</div>
             <div className="Project_Tag">우리의,<br />우수 프로젝트</div>
             <div className="bar"></div>
             <div className="Project_Container d-flex justify-content-center w-100">
                 <div className="row">
-                    <ProjectCard img="assets/temp_image.png" title="1111111" />
-                    <ProjectCard img="assets/temp_image.png" title="22222222" />
-                    <ProjectCard img="assets/temp_image.png" title="33333333" />
+                    <ProjectCard img="assets/temp_image.png" title="어떤어떤프로젝트" id='1' onClick={() => history.push("blogs")} />
+                    <ProjectCard img="assets/temp_image.png" title="어떤어떤프로젝트" id='2' onClick={() => history.push("blogs")} />
+                    <ProjectCard img="assets/temp_image.png" title="어떤어떤프로젝트" id='3' onClick={() => history.push("blogs")} />
                 </div>
             </div>
         </div>
