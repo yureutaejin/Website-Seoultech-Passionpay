414c37ca08b58762fcf394795c12219be2510503
unknown <2gnldud@gmail.com>
2022-11-16 11:44:35 +0900

    2022-11-16-11-43-HY

--- project/frontend/front_app/src/components/ProjectCard/ProjectCard.js
+++ project/frontend/front_app/src/components/ProjectCard/ProjectCard.js
@@ -1,15 +1,15 @@
-import "./ProjectCard.css"
+import styles from "./ProjectCard.module.css"
 import classNames from "classnames/bind"
 
-// const cx = classNames.bind(styles)
+const cx = classNames.bind(styles)
 
 const ProjectCard = ({ id, title, img, onClick }) => {
     return (
-        <div className="card col-4 m-3 px-0 cursor-pointer" onClick={onClick} >
-            <div className="card-body">
-                <h5 className="card-title"><div className="mb-1">{id}</div>{title}</h5>
+        <div className={cx("card", "col-4", "m-3", "px-0", "cursor-pointer")} onClick={onClick} >
+            <div className={cx("card-body")}>
+                <h5 className={cx("card-title")}><div className="mb-1">{id}</div>{title}</h5>
             </div>
-            <img src={img} className="card-img-bottom" alt="assets/temp_image.png" />
+            <img src={img} className={cx("card-img-bottom", "mt-2")} alt="assets/temp_image.png" />
         </div>
     )
 }
--- project/frontend/front_app/src/pages/HomePage/HomePage.js
+++ project/frontend/front_app/src/pages/HomePage/HomePage.js
@@ -1,17 +1,19 @@
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
-import "./HomePage.scss";
+import styles from "./HomePage.module.css";
 import { useHistory } from "react-router"
+import classNames from "classnames/bind"
 
 const HomePage = () => {
     const history = useHistory()
+    const cx = classNames.bind(styles)
     return (
         <div>
-            <img src="assets/HomeIMG.png" alt="assets/temp_image.png" className="HomeIMG w-100"></img>
-            <img src="assets/HomeLogo.svg" alt="assets/temp_image.png" className="HomeLogo"></img>
-            <div className="HomeDesc">서울과학기술대학교<br /><span className="fw-bold">산업정보시스템전공</span> 학술 동아리</div>
-            <div className="Project_Tag">우리의,<br />우수 프로젝트</div>
-            <div className="bar"></div>
-            <div className="Project_Container d-flex justify-content-center w-100">
+            <img src="assets/HomeIMG.png" alt="assets/temp_image.png" className={cx("HomeIMG", "w-100")}></img>
+            <img src="assets/HomeLogo.svg" alt="assets/temp_image.png" className={cx("HomeLogo")}></img>
+            <div className={cx("HomeDesc")}>서울과학기술대학교<br /><span className="fw-bold">산업정보시스템전공</span> 학술 동아리</div>
+            <div className={cx("Project_Tag")}>우리의,<br />우수 프로젝트</div>
+            <div className={cx("bar")}></div>
+            <div className={cx("Project_Container", "d-flex", "justify-content-center", "w-100")}>
                 <div className="row">
                     <ProjectCard img="assets/temp_image.png" title="어떤어떤프로젝트" id='1' onClick={() => history.push("blogs")} />
                     <ProjectCard img="assets/temp_image.png" title="어떤어떤프로젝트" id='2' onClick={() => history.push("blogs")} />
