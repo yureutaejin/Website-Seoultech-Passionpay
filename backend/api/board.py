7d66effa74e5f21b87058f8885fc634beabab901
yureutaejin <jyjin3@me.com>
2022-12-01 13:29:27 +0900

    add get one board

--- backend/api/board.py
+++ backend/api/board.py
@@ -1,10 +1,13 @@
 from sqlalchemy.orm import Session
-from models.model import Board
+from models.model import Board, Sign
 from models.pydantic_model import Board_post_pydantic
 from datetime import datetime
 
 def get_boards(db: Session):
-    return db.query(Board).all()
+    return db.query(Board.subject, Board.date, Board.sign_id, Sign.user_name).join(Sign).all()
+
+def get_one_board(board_number: int, db: Session):
+    return db.query(Board.subject, Board.date, Board.sign_id, Sign.user_name).join(Sign).filter(Board.id==board_number).all()
 
 def post_board(db: Session, post_form: Board_post_pydantic):
     try:
--- backend/routers/board_route.py
+++ backend/routers/board_route.py
@@ -18,6 +18,14 @@ def get_boards(db: Session = Depends(get_db)):
         "res" : res
     }
     
+@router.get("/{board_number}")
+def get_one_board(board_number: int, db: Session = Depends(get_db)):
+    res = board.get_one_board(db=db, board_number=board_number)
+    
+    return {
+        "res" : res
+    }
+    
 @router.post("/")
 def post_board(post_form: Board_post_pydantic, db: Session = Depends(get_db)):
     res = board.post_board(db=db, post_form=post_form)
